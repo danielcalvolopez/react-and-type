38e76db58c516dd2df58a1d55f9104a61b920c12
Daniel <acidistrict@gmail.com>
2022-11-13 23:38:39 +0000

    create todos component

---
+++ src/components/Todos.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Todos = () => {
+  return (
+    <ul>
+      <li>React</li>
+      <li>Typescript</li>
+      <li>CSS modules</li>
+    </ul>
+  );
+};
+
+export default Todos;
